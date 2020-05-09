c858bf1bb10b52557de947e9f77dcd71998b0a87
Karnav Patel <46722699+karnav-patel@users.noreply.github.com>
2020-05-09 15:05:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
-from flask import Flask ,request,jsonify
+from flask import Flask , request , jsonify
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
+
 @app.route('/api',methods=['GET'])
 def hello_world():
-    d={}
-    a={}
-    b={}
-    f={}
-    s={}
-
-    d['name'] = str(request.args['name'])
-    if d['name'] == 'karnav':
-        a['see'] = 'Your father name is Sharadkumar'
-        return jsonify(a)
-
-    elif d['name'] == 'vrushik':
-        b['see'] = 'Your father name is Sudhirkumar'
-        return jsonify(b)
-        
-    elif d['name'] == 'simreen':
-        s['see'] = 'Your father name is InderSingh and mother name is SatinderKaur. IMP: Simreen is Fool'
-        return jsonify(s)
-        
-    else:
-        f['see'] = 'Your father name is something else'
-        return jsonify(f)
+    l = []
+    c={}
+    uri = 'https://en.m.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic_by_country_and_territory'
+      
+    content = requests.get(uri).content
+    soup = BeautifulSoup(content, 'html.parser')
+    table = soup.find('table', {'class': 'wikitable plainrowheaders sortable'}).tbody
+    rows = table.find_all('tr')
+    c['country'] = str(request.args['country'])
     
+    for i in range(2,len(rows)-2):
+            d = {}
+            temp = rows[i].find_all('th')
+            OverAllCases = rows[i].find_all('td')
+            coutryName = temp[1].find('a')
+            if c['country'] == coutryName.text:
+                #d['Countries'] = coutryName.text
+                d['TotalCases'] = OverAllCases[0].text.replace('\n','')
+                #d['Deaths'] = OverAllCases[1].text.replace('\n','')
+                #d['Recov'] = OverAllCases[2].text.replace('\n','')
+                l.append(d)
+                return jsonify(d)
+
+                
+
+    return jsonify(d)
 
 
 if __name__ == '_main_':
